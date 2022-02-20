4bf48c4737e0fc886d5c700665a4e2c1b38afcc4
KhaledKhm <47531479+KhaledKhm@users.noreply.github.com>
2022-02-20 15:49:17 +0100

    start of crud

---
+++ TechGeeks/TechGeeks/src/main/java/tn/esprit/spring/service/PotServiceImpl.java
@@ -0,0 +1,12 @@
+package tn.esprit.spring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PotServiceImpl implements IPotService{
+
+	public PotServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
