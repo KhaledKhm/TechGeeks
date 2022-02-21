1a1f6f532b269b9350db90f6d84f3614afd9867d
ahmedba <35056619+Ahmedbannour@users.noreply.github.com>
2022-02-22 00:59:30 +0100

    offreController

---
+++ TechGeeks/TechGeeks/src/main/java/tn/esprit/spring/controller/OffreController.java
@@ -0,0 +1,62 @@
+package tn.esprit.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.entities.Offre;
+import tn.esprit.spring.service.IOffreService;
+
+
+@RestController
+public class OffreController {
+
+	@Autowired
+	IOffreService offreService;
+	
+	public OffreController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@PostMapping("/add-offre")
+	@ResponseBody
+	Offre addOffre(@RequestBody Offre o){
+		return offreService.addOffre(o);
+	}
+	
+	@GetMapping("/retrieve-allOffres")
+	@ResponseBody
+	List<Offre> retrieveAllOffres(){
+		
+		return offreService.retrieveAllOffres();
+	}
+	
+	@GetMapping("/get-Offre/{idOffre}")
+	@ResponseBody
+	Offre getOffreById(@PathVariable("idOffre") int idOffre){
+		return offreService.retrieveOffre(idOffre);
+		
+	}
+	
+	@PutMapping("modify-offre")
+	@ResponseBody
+	Offre updateOffre(@RequestBody Offre o){
+		
+		return offreService.updateOffre(o);
+	}
+	
+	@DeleteMapping("delete-offre/{id}")
+	void deleteEvent(@PathVariable("id") int idOffre){
+		offreService.deleteOffre(idOffre);
+		
+	}
+
+}
