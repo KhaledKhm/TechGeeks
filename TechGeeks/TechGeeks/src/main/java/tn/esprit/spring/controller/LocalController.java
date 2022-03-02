c73047ab651dca42706496a230a161b685682fac
kridhamza <79025642+kridhamza@users.noreply.github.com>
2022-03-02 14:39:28 +0100

    updates

--- TechGeeks/TechGeeks/src/main/java/tn/esprit/spring/controller/LocalController.java
+++ TechGeeks/TechGeeks/src/main/java/tn/esprit/spring/controller/LocalController.java
@@ -1,10 +1,17 @@
 package tn.esprit.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.service.ILocalService;
+
+
 
 @RestController
 public class LocalController {
 
+	@Autowired
+	ILocalService localService;
 	public LocalController() {
 		// TODO Auto-generated constructor stub
 	}
